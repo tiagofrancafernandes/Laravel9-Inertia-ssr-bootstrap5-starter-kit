d15cbda187d57a342bbcd29215902c21af2d1b09
Tiago França <tiago@tiagofranca.com>
2023-07-13 12:18:13 -0300

    modified:   database/seeders/RolesPermissionsTableSeeder.php

--- database/seeders/RolesPermissionsTableSeeder.php
+++ database/seeders/RolesPermissionsTableSeeder.php
@@ -16,25 +16,25 @@ class RolesPermissionsTableSeeder extends Seeder
      */
     public function run()
     {
-		// Reset cached roles and permissions
+        // Reset cached roles and permissions
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-		// create permissions
-		Permission::create(['name' => 'view users', 'guard_name' => 'web']);
+        // create permissions
+        Permission::create(['name' => 'view users', 'guard_name' => 'web']);
         Permission::create(['name' => 'edit users', 'guard_name' => 'web']);
         Permission::create(['name' => 'delete users', 'guard_name' => 'web']);
         Permission::create(['name' => 'create users', 'guard_name' => 'web']);
         Permission::create(['name' => 'assign roles', 'guard_name' => 'web']);
         Permission::create(['name' => 'assign permissions', 'guard_name' => 'web']);
 
-		// create roles and assign created permissions
+        // create roles and assign created permissions
         $role2 = Role::create(['name' => 'admin'])
-					->givePermissionTo(Permission::all());
+            ->givePermissionTo(Permission::all());
 
-		$role1 = Role::create(['name' => 'user'])
-					->givePermissionTo(['view users']);
+        $role1 = Role::create(['name' => 'user'])
+            ->givePermissionTo(['view users']);
 
-		// create dummy users
+        // create dummy users
         $user = \App\Models\User::factory()->create([
             'name' => 'Example User',
             'email' => 'user@example.com',
